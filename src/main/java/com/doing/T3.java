97ea51d6b258c0509c158fdfb761dd652525df16
815245721 <815245721@qq.com>
2022-01-12 21:56:16 +0800

    c

--- src/main/java/com/doing/T3.java
+++ src/main/java/com/doing/T3.java
@@ -1,8 +1,18 @@
 package com.doing;
 //T3
+//ccc
 //注释3
 public class T3 {
+    private String name;
     public static void main(String[] args) {
 
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
