b47c4dd1555918b3a72b59757a64d6453176fe1f
815245721 <42689394+815245721@users.noreply.github.com>
2022-07-16 14:05:59 +0800

    Update GitDemo.java
    rebase1

--- src/main/java/com/doing/GitDemo.java
+++ src/main/java/com/doing/GitDemo.java
@@ -3,7 +3,7 @@ package com.doing;
 public class GitDemo {
     public static void main(String[] args) {
         //测试一下
-        System.out.println("66");
+        System.out.println("88");
 
         //提交1
         //提交3
