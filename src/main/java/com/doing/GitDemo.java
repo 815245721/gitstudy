29a316d6a39e7cf55982016d75b6bd2f23d43556
815245721 <42689394+815245721@users.noreply.github.com>
2022-07-16 14:18:51 +0800

    remote2

--- src/main/java/com/doing/GitDemo.java
+++ src/main/java/com/doing/GitDemo.java
@@ -3,7 +3,7 @@ package com.doing;
 public class GitDemo {
     public static void main(String[] args) {
         //测试一下
-        System.out.println("remote1");
+        System.out.println("remote2");
 
         //提交1
         //提交3
